d137aca15b9c52ecf827fec91799d25dea9c3337
kshitij_pandey <kshitijpandey32@gmail.com>
2016-07-14 13:54:35 +0530

    completed upload page with notification

--- post.php
+++ post.php
@@ -24,14 +24,18 @@
 		$insertQuery = "INSERT INTO missing_pers (name,gender,age,image_path,date_missing,info) VALUES ('$fullName', '$gender','$age','$path','$dateOfMissing','$info')";
 		$insertResult = mysqli_query($conn,$insertQuery);
 		if($insertResult){
-			echo "DONE";
+			echo '<div class="alert alert-success fade in">
+					<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+  					<strong>Success!</strong> Information posted successfully.
+					</div>';
 		}
 		else{
-			echo mysqli_error($conn);
+			echo '<div class="alert alert-danger fade in">
+					<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+  					<strong>Failed!</strong> Somethig went wrong. Please try again.
+					</div>';
 		}
 	}
-	
-	
 ?>
 <head>
 	<title>Post a missing person</title>
@@ -40,18 +44,18 @@
 	<div class="container">
 		<form id="postForm" action="" method="post" class="form-group" enctype="multipart/form-data">
 	    	<label for="name">Full Name:</label>
-	        <input type="text" class="form-control" name="fullName" id="fullName">
+	        <input type="text" class="form-control" name="fullName" id="fullName" required>
 	        <label for="name">Age:</label>
-	        <input type="number" maxlength="100" name="age" class="form-control" id="age">
+	        <input type="number" maxlength="100" name="age" class="form-control" id="age" required>
 	        <label for="name">Gender:</label>
-	        <input type="radio" name="gender" value="male"><br>
-	        <input type="radio" name="gender" value="female"><br>
+	        <input type="radio" name="gender" value="male">Male&nbsp;
+	        <input type="radio" name="gender" value="female">Female<br>
 	        <label for="name">Image</label>
-	        <input type="file" name="image" class="form-control" id="image"><br>  
+	        <input type="file" name="image" class="form-control" id="image" required><br>  
 	        <label for="name">Date of Missing:</label>
-	        <input type="date" name="dom" class="form-control" id="date">
+	        <input type="date" name="dom" class="form-control" id="date" required>
 	        <label for="name">General Information</label>
-	        <textarea type="text" name="info" class="form-control" id="info"></textarea>
+	        <textarea type="text" name="info" class="form-control" id="info" required></textarea>
 	        <button class="btn btn-success" name="submit" id="submit">Submit</button>
 	   	</form>
 	</div>
